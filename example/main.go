ee1653ae3162f0ceb45a28b7a7c4f7590b99f843
Kevin C. Krinke <kevin@krinke.ca>
2022-11-01 14:42:39 -0400

    [go-enjin]: renaming package to go-enjin fork (for testing btimes-linux changeset and remote build systems)

--- example/main.go
+++ example/main.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/djherbis/times"
+	"github.com/go-enjin/github-com-djherbis-times"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/djherbis/times
+module github.com/go-enjin/github-com-djherbis-times
 
 go 1.16
